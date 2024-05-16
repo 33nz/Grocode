18182f10c4b54cfeb1ff5b25bcdaa86c1c573729
Jack Wilson <jxwilx@gmail.com>
2024-05-17 10:42:14 +1200

    form submission working

--- client/components/NewChallenge.tsx
+++ client/components/NewChallenge.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const NewChallenge = () => {
+  //useState for challenge description and dueDate
   const [challenge, setChallenge] = useState('')
   const [dueDate, setDueDate] = useState('')
+
+  //onsubmit to submit form as new challenge object, WIP
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault()
+      console.log('submitted')
+    } catch (err: any) {
+      console.error(err)
+    }
+  }
+
+  //challenge and duedate as input fields
   return (
     <div>
       <h2>In this challenge...</h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           required
