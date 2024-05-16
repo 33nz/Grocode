6db232d27c089f3b88594d86bec5646025e12d18
Corina Reitemeyer <corina@corina.org>
2024-05-17 10:56:08 +1200

    Import Bulma library into css file

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,13 +3,6 @@ import OngoingChallenge from './OngoingChallenge'
 function App() {
   return (
     <>
-      <div className="App-background">
-        <div className="hero is-fullheight has-background-primary-00">
-          <div className="hero-body">
-            <div className="container"></div>
-          </div>
-        </div>
-      </div>
       <OngoingChallenge />
     </>
   )
--- client/components/OngoingChallenge.tsx
+++ client/components/OngoingChallenge.tsx
@@ -1,13 +1,5 @@
 function OngoingChallenge() {
   return (
-    <div>
-      <h1 className="is-size-4">Challenge input goes here</h1>
-      <p className="is-size-5 has-text-light">Due on</p>
-      <p className="is-size-6">24 May 2024</p>
-      <progress className="progress is-primary" value="30" max="100">
-        30%
-      </progress>
-    </div>
   )
 }
 
